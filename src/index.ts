0c37a0402241737d6eb877fdbec42568a793625a
Jack Chakany <jacany@chaker.net>
2022-11-22 09:43:11 -0500

    get bot parts down

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+import {
+	Client,
+	Events,
+	GatewayIntentBits,
+	SlashCommandBuilder,
+} from "discord.js";
+const { token } = require("../config.json");
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (c) => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(token);
