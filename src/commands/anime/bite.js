fe3f8372fa17e5331b917b8ba576193dca8cd462
Eric Kang <kang.eric.hi@gmail.com>
2022-12-01 10:59:18 -0500

    test

--- src/commands/anime/bite.js
+++ src/commands/anime/bite.js
@@ -1,15 +1,15 @@
 const Command = require("../../structures/Command.js");
 const { request } = require("undici");
 
-class Hug extends Command {
+class Bite extends Command {
   constructor(...args) {
     super(...args, {
-      description: "Hug someone else.",
-      usage: "hug [user]",
+      description: "Bite someone else.",
+      usage: "bite [user]",
       options: [
         {
           name: "user",
-          description: "The user you want to hug.",
+          description: "The user you want to bite.",
           type: "user",
         },
       ],
@@ -18,17 +18,17 @@ class Hug extends Command {
   
   async run(ctx, options) {
     const user = options.getUser("user") || ctx.author;
-    const { url } = await request("https://nekos.life/api/v2/img/hug")
+    const { url } = await request("https://api.waifu.pics/sfw/bite")
       .then(({ body }) => body.json());
     const embed = this.client
       .embed(ctx.author)
-      .setTitle(`Hug!`)
+      .setTitle(`Bite >:(`)
       .setImage(url)
-    if (user.id !== ctx.author.id) embed.setDescription(`**${ctx.author.username}** is hugging **${user.username}**!`)
+    if (user.id !== ctx.author.id) embed.setDescription(`**${ctx.author.username}** is biting **${user.username}**! That must have hurt...`)
     return ctx.reply({ embeds: [embed] });
       
   }
 }
 
 
-module.exports = Hug;
+module.exports = Bite;
