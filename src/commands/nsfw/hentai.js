e242687e4826bc9ea2b433442cb909b65b57e837
Eric Kang <kang.eric.hi@gmail.com>
2022-12-01 13:26:52 -0500

    test

---
+++ src/commands/nsfw/hentai.js
@@ -0,0 +1,27 @@
+
+const Command = require("../../structures/Command.js");
+const { EmbedBuilder } = require("discord.js");
+const fetch = require("node-fetch");
+
+class Hentai extends Command {
+  constructor(...args) {
+    super(...args, {
+      description: "[NSFW] Hentai.",
+      nsfw: true
+    });
+  }
+
+  async run(ctx, options) {
+    const user = options.getUser("user") || ctx.author;
+    const data = await request("https://api.waifu.im/random/?selected_tags=hentai&gif=true")
+      .then(({ body }) => body.json());
+    const embed = this.client
+      .embed(ctx.author)
+      .setTitle("Hentai :eggplant:")
+      .setImage(data["images"][0]["url"])
+    return ctx.reply({ embeds: [embed] });
+      
+  }
+}
+
+module.exports = Hentai;
