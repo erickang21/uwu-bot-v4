fd5273dafb0392f02abc3dd0f9cf813e873a66c6
Eric Kang <kang.eric.hi@gmail.com>
2023-08-03 16:15:44 -0400

    add a command to test if nekobot works

---
+++ src/commands/images/blurpify.js
@@ -0,0 +1,33 @@
+const Command = require("../../structures/Command.js");
+const { request } = require("undici");
+
+class Blurpify extends Command {
+  constructor(...args) {
+    super(...args, {
+      description: "Blurpify someone's profile picture!",
+      usage: "blurpify @user",
+      options: [
+        {
+          name: "user",
+          description: "The user you want to kiss.",
+          type: "user",
+        },
+      ],
+    });
+  }
+
+  // Image Based
+  async run(ctx, options) {
+    const user = options.getUser("user") || ctx.author;
+    const res = await request(`https://nekobot.xyz/api/imagegen?type=blurpify&image=${user.displayAvatarURL({ size: 1024, format: "png", dynamic: true })}`)
+      .then(({ body }) => body.json());
+    if (!res.success) return ctx.reply("An unexpected error occurred with the API.");
+    const embed = this.client.embed(user)
+      .setTitle("Blurpify")
+      .setImage(res.message)
+    return ctx.reply({ embed });
+  }
+}
+
+
+module.exports = Blurpify;
