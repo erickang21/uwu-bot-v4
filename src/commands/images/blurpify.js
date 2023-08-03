38ebbb7a00f361f5a64bcd571ff10690609d628a
Eric Kang <kang.eric.hi@gmail.com>
2023-08-03 17:42:21 -0400

    see if nodefetch fixes it

--- src/commands/images/blurpify.js
+++ src/commands/images/blurpify.js
@@ -1,5 +1,5 @@
 const Command = require("../../structures/Command.js");
-const { request } = require("undici");
+const fetch = require("node-fetch");
 
 class Blurpify extends Command {
   constructor(...args) {
@@ -19,9 +19,8 @@ class Blurpify extends Command {
   // Image Based
   async run(ctx, options) {
     const user = options.getUser("user") || ctx.author;
-    const res = await request(`https://nekobot.xyz/api/imagegen?type=blurpify&image=${user.displayAvatarURL({ size: 1024, extension: "png", dynamic: true })}`)
-      .then(({ body }) => body.json());
-    console.log(res);
+    const res = await fetch(`https://nekobot.xyz/api/imagegen?type=blurpify&image=${user.displayAvatarURL({ size: 1024, extension: "png", dynamic: true })}`)
+      .then((body) => body.json());
     if (!res.success) return ctx.reply("An unexpected error occurred with the API.");
     const embed = this.client.embed(user)
       .setTitle("Blurpify")
--- src/commands/nsfw/genshin.js
+++ src/commands/nsfw/genshin.js
@@ -21,11 +21,12 @@ class Genshin extends Command {
   }
 
   async run(ctx, options) {
+    const blacklistedCharacters = [];
     const characterName = options.getString("character");
     const data = await request(
       `https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&tags=${characterName.toLowerCase().replaceAll(" ", "_")}_(genshin_impact)%20genshin_impact%20-loli%20rating:explicit`
     ).then(({ body }) => body.json());
-    if (!data.post?.length) return ctx.reply(`No results were found. ${emojis.failure}`);
+    if (!data.post?.length || blacklistedCharacters.includes(characterName.toLowerCase())) return ctx.reply(`No results were found. ${emojis.failure}`);
     const urls = data.post.map((entry) => entry.file_url)
     const embed = this.client
       .embed(ctx.author)
