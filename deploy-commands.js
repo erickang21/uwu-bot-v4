f6bef28570661e21f7be7687695d4ec8437f56ae
Eric Kang <kang.eric.hi@gmail.com>
2023-08-08 23:51:49 -0400

    change the require path for this

--- deploy-commands.js
+++ deploy-commands.js
@@ -6,7 +6,7 @@ const { TOKEN_DEV, TOKEN, DEV, CLIENT_ID } = process.env;
 
 const rest = new REST({ version: "10" }).setToken(DEV ? TOKEN_DEV : TOKEN);
 
-const UwUClient = require("./structures/UwUClient.js");
+const UwUClient = require("./src/structures/UwUClient");
 
 // Create a client but do not log in, just for loading commands.
 const client = new UwUClient();
