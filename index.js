847a40cdac87c4373b5d19d77101b38f1f726dff
Eric <33041238+erickang21@users.noreply.github.com>
2024-05-10 11:03:58 -0400

    Remove timeout

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ require("dotenv").config();
 const { ShardingManager } = require("discord.js");
 const token = process.env.NODE_ENV == "development" ? process.env.TOKEN_DEV : process.env.TOKEN;
 
-const manager = new ShardingManager("./src/index.js", { token });
+const manager = new ShardingManager("./src/index.js", { 
+  token, 
+  totalShards: "auto",
+  timeout: -1,
+  respawn: true 
+});
 
 manager.on("shardCreate", (shard) => {
   console.log(`Launched shard ${shard.id}`);
 });
 
-manager.spawn();
+manager.spawn({ amount: 'auto', delay: 5500, timeout: 30000 }).catch(e => console.log(`An error occured with spawning a shard. ${e}`));
