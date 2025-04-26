6b11489f004406ee132277ece58d45ba1b31add6
Eric <33041238+erickang21@users.noreply.github.com>
2025-04-26 00:19:13 -0400

    Fix config logic

--- src/commands/customization/command.js
+++ src/commands/customization/command.js
@@ -52,11 +52,11 @@ class ManageCommands extends Command {
 
   async run(ctx, options) {
     if(!ctx.member.permissions.has("MANAGE_GUILD"))
-      return ctx.reply(`Baka! You need the \`Manage Server\` permissions to change modlogs. ${emojis.failure}`);
+      return ctx.reply(`Baka! You need the \`Manage Server\` permissions to change this. ${emojis.failure}`);
     const guildSettings = await this.client.syncGuildSettingsCache(ctx.guild.id);
     let option = options.getString("action");
     let command = options.getString("command");
-    let roles = [1,2,3,4,5].map((i) => options.getRole(`role${i}`));
+    let roles = [1,2,3,4,5].map((i) => options.getRole(`role${i}`).replace("<@", "").replace(">", ""));
     roles = roles.filter((x) => x); // Filter out any that are unused.
 
 
@@ -77,6 +77,7 @@ class ManageCommands extends Command {
     } else if (option === "enable") {
       // Make sure they specified a command.
       if (!command) return ctx.reply("You need to specify the command to be changed!");
+      else if (command.toLowerCase() === "command") return ctx.reply("You cannot modify access to this command.");
       else if (!this.client.commands.has(command)) return ctx.reply("This is not a valid command of uwu bot.");
       // GET CONFIG INFORMATION
       const originalCommandConfig = originalConfig[command] || { use: "all", roles: {} };
@@ -98,6 +99,7 @@ class ManageCommands extends Command {
     } else if (option === "disable") {
       // Make sure they specified a command.
       if (!command) return ctx.reply("You need to specify the command to be changed!");
+      else if (command.toLowerCase() === "command") return ctx.reply("You cannot modify access to this command.");
       else if (!this.client.commands.has(command)) return ctx.reply("This is not a valid command of uwu bot.");
       // GET CONFIG INFORMATION
       const originalCommandConfig = originalConfig[command] || { use: "all", roles: {} };
