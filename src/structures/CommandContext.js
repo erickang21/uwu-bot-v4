701009aa056c2b46d5dc0eafd6aa0534f905c058
Eric <33041238+erickang21@users.noreply.github.com>
2025-04-28 15:55:27 -0400

    Fix slash issues

--- src/structures/CommandContext.js
+++ src/structures/CommandContext.js
@@ -77,7 +77,8 @@ class CommandContext {
   }
 
   async reply(options) {
-    if (this.slash) return this.interaction.reply(options);
+    if (this.slash && !this.interaction.replied) return this.interaction.reply(options);
+    if (this.slash) return this.interaction.editReply(options);
     if (this.message.lastReply) return this.message.lastReply.edit(options);
     this.message.lastReply = await this.message.reply(options);
     return this.message.lastReply;
